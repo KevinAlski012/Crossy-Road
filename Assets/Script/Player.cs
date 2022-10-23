969644fe484efe378386da209b260c440c838ec9
KevinAlski012 <kevinyonatan47@gmail.com>
2022-10-23 20:39:18 +0700

    50%

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private TerrainGenerator terrainGenerator;
+    private Animator animator;
+    private bool isHopping;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        Debug.Log(Input.GetKeyDown(KeyCode.W) && !isHopping);
+        if (Input.GetKeyDown(KeyCode.W) && !isHopping)
+        {
+            float zDifference = 0;
+            if(transform.position.z % 1 != 0)
+            {
+                zDifference = Mathf.Round(transform.position.z) - transform.position.z;
+            }
+            MoveCharacter(new Vector3(1, 0, zDifference));
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && !isHopping)
+        {
+            MoveCharacter(new Vector3(0, 0, 1));
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && !isHopping)
+        {
+            MoveCharacter(new Vector3(0, 0, -1));
+        }
+    }
+
+    public void MoveCharacter(Vector3 difference)
+    {
+        animator.SetTrigger("hop");
+        isHopping = true;
+        transform.position = (transform.position + difference);
+        terrainGenerator.SpawnTerrain(false, transform.position);
+    }
+    public void FinishHop()
+    {
+        isHopping = false;
+    }
+}
